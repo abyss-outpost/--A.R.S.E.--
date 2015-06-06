7395f32230bcdcdf690739c956b30dca6c6b3e5c
Maeoll Kim <kim38@rohan.sdsu.edu>
2015-06-06 05:38:03 -0700

    Update data.js
    tabs<>spaces

--- example/scripts/Data.js
+++ example/scripts/Data.js
@@ -1,8 +1,8 @@
 export class Data {
-  									constructor($http){
-    																		this.$http = $http;
-  																		}
-  									load()						{
-    																		return this.$http.get('/data/data.json').then((res) => res.data);
-  																		}
-									}
+                    constructor($http){
+                                        this.$http = $http;
+                                      }
+                    load()            {
+                                        return this.$http.get('/data/data.json').then((res) => res.data);
+                                      }
+                  }
